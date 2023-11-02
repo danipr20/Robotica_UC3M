3313a4df12a2aa81587203f22bbb6a6d48bf8ffd
Daniel <danipr2000@gmail.com>
2023-11-02 15:29:02 +0100

    subida de p1 y p2 a terminar

---
+++ Prieto_Daniel_P2/controllers/Steaight_compass/Steaight_compass.cpp
@@ -0,0 +1,17 @@
+
+#include "MyRobot.h"
+
+/**
+ * @brief Main program.
+ */
+int main(int argc, char **argv)
+{
+    MyRobot* my_robot = new MyRobot();
+
+    my_robot->run();
+
+    delete my_robot;
+
+    return 0;
+}
+
---
+++ Prieto_Daniel_P2/controllers/compass_example/build/release/compass_example.d
@@ -0,0 +1,6 @@
+build/release/compass_example.o: compass_example.cpp MyRobot.h \
+ C:\Users\danip\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Users\danip\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Users\danip\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Users/danip/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
+ C:\Users\danip\AppData\Local\Programs\Webots/include/controller/cpp/webots/Compass.hpp
